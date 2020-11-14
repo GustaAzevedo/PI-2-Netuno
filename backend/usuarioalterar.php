0e10cfe6dd566395fa7a23e253abfc8d0b6d65c5
Guilherme Pereira de Castro <63517384+GuilhermePCastro@users.noreply.github.com>
2020-11-14 09:39:24 -0300

    Ajustes para backend

---
+++ backend/usuarioalterar.php
@@ -0,0 +1,100 @@
+<?php 
+session_start();
+include_once "./config/db.php";
+
+// validando usuário
+if($_SESSION['idusuario'] = 0){
+    header('Location: ../web/src/views/pg-login.html');
+    exit();
+}
+
+// verificando se é uma alteração   
+if(isset($_POST['pk_id'])){
+
+    $id = preg_replace('/\D/','', $_POST['pk_id']);
+    //pegando variaveis
+    $login    = $_POST['ds_login'];
+    $senha    = $_POST['ds_senha'];
+    $senhacon = $_POST['ds_senhacon'];
+    $adm      = $_POST['tg_adm'] ?? 0;
+
+    var_dump($_POST);
+    
+    if($senha != $senhacon){
+
+        //montando o registro para alterar
+        $query = "SELECT * FROM TS_USUARIO WHERE PK_ID = $id";
+        $result = $objBanco -> query($query);
+        $array = $result -> fetch(PDO::FETCH_ASSOC);
+
+        //substituindo os valores para continuar com o que foi digitado
+        $array['DS_LOGIN']  = $login;
+        $array['TG_ADM']    = $adm ;
+
+        //passando para a tela
+        $_SESSION['erro'] = true;
+        $_SESSION['msgusu'] = 'As senhas não são iguais!';
+        include "../web/src/views/usuarioalterar.php";
+        exit();
+    }
+
+
+    //verificando login
+    $objSmtm = $objBanco -> prepare("SELECT DS_LOGIN FROM TS_USUARIO WHERE DS_LOGIN = :LOGIN AND PK_ID <> $id");
+    $objSmtm -> bindparam(':LOGIN',$login);
+    $objSmtm -> execute();
+    $result = $objSmtm -> fetch(PDO::FETCH_ASSOC);
+    // se cair aqui, já existe cadastrado
+    if($result){
+        
+        //montando o registro para alterar
+        $query = "SELECT * FROM TS_USUARIO WHERE PK_ID = $id";
+        $result = $objBanco -> query($query);
+        $array = $result -> fetch(PDO::FETCH_ASSOC);
+
+        //substituindo os valores para continuar com o que foi digitado
+         $array['DS_LOGIN']  = $login;
+         $array['TG_ADM']    = $adm ;
+ 
+        //passando para a tela
+        $_SESSION['erro'] = true;
+        $_SESSION['msgusu'] = 'Login já cadastrado!';
+        include "../web/src/views/usuarioalterar.php";
+        
+        exit();
+    }
+
+
+    $objSmtm = $objBanco -> prepare("UPDATE TS_USUARIO SET
+                                        DS_LOGIN = :DS_LOGIN, 
+                                        DS_SENHA = md5(:DS_SENHA), 
+                                        TG_ADM   = :TG_ADM,
+                                      DH_ALTERACAO = now()
+                                    WHERE
+                                        PK_ID = $id");
+
+    $objSmtm -> bindparam(':DS_LOGIN',$login);
+    $objSmtm -> bindparam(':DS_SENHA',$senha);
+    $objSmtm -> bindparam(':TG_ADM',intval($adm),PDO::PARAM_INT);
+    
+    if($objSmtm -> execute()){
+        header('Location: ./usuarioconsultar.php');
+        $_SESSION['erro'] = false;
+        $_SESSION['msgusu'] = 'Registro alterado com sucesso!';
+        exit(); 
+    }else{
+        header('Location: ./usuarioconsultar.php'); 
+        $_SESSION['erro'] = true;
+        $_SESSION['msgusu'] = 'Erro ao alterar o cadastro, tente novamente mais tarde!';
+        exit();
+    }
+
+}else{
+
+    $id = preg_replace('/\D/','', $_GET['id']);
+    $query = "SELECT * FROM TS_USUARIO WHERE PK_ID = $id";
+    $result = $objBanco -> query($query);
+    $array = $result -> fetch(PDO::FETCH_ASSOC);
+
+    include "../web/src/views/usuarioalterar.php";
+}
---
+++ backend/usuariodigitar.php
@@ -0,0 +1,84 @@
+<?php
+session_start();
+include_once "./config/db.php";
+
+// validando usuário
+if($_SESSION['idusuario'] = 0){
+    header('Location: ../web/src/views/pg-login.html');
+    exit();
+}
+
+include "./functions/valida_user.php";
+
+//pegando variaveis
+$login    = $_POST['ds_login'];
+$email    = $_POST['ds_email'];
+$senha    = $_POST['ds_senha'];
+$senhacon = $_POST['ds_senhacon'];
+$adm      = $_POST['tg_adm'] ?? 0;
+
+
+
+if($senha != $senhacon){
+    header('Location: ../web/src/views/usuario.php'); 
+    $_SESSION['erro'] = true;
+    $_SESSION['msgusu'] = 'As senhas não são iguais!';
+    exit();
+}
+
+//verificando login
+$objSmtm = $objBanco -> prepare("SELECT DS_LOGIN FROM TS_USUARIO WHERE DS_LOGIN = :LOGIN");
+$objSmtm -> bindparam(':LOGIN',$login);
+$objSmtm -> execute();
+$result = $objSmtm -> fetch(PDO::FETCH_ASSOC);
+// se cair aqui, já existe cadastrado
+if($result){
+    header('Location: ../web/src/views/usuario.php'); 
+    $_SESSION['erro'] = true;
+    $_SESSION['msgusu'] = 'Login já cadastrado!';
+    exit();
+}
+
+
+//verificando email
+$objSmtm = $objBanco -> prepare("SELECT DS_EMAIL FROM TS_USUARIO WHERE DS_EMAIL = :EMAIL");
+$objSmtm -> bindparam(':EMAIL',$email );
+$objSmtm -> execute();
+$result = $objSmtm -> fetch(PDO::FETCH_ASSOC);
+
+// se cair aqui, já existe cadastrado
+if($result){
+    header('Location: ../web/src/views/usuario.php'); 
+    $_SESSION['erro'] = true;
+    $_SESSION['msgusu'] = 'E-mail já cadastrado!';
+    exit();
+}
+
+//query de insert
+$queryInsert = "insert into ts_usuario (DS_LOGIN, DS_EMAIL, DS_SENHA, TG_ADM, DH_INCLUSAO, FK_USUCRIADOR) 
+                                        values (:ds_login, :ds_email,  md5(:ds_senha), :tg_adm, now(), :fk_usucriador)";
+
+//preparando query
+$objSmtm = $objBanco -> prepare($queryInsert);
+
+// substituindo os valores
+$objSmtm -> bindparam(':ds_login',$login);
+$objSmtm -> bindparam(':ds_email',$email);
+$objSmtm -> bindparam(':ds_senha',$senha);
+$objSmtm -> bindparam(':tg_adm',intval($adm),PDO::PARAM_INT);
+$objSmtm -> bindparam(':fk_usucriador',intval($_SESSION['usersessao']['idusuario']),PDO::PARAM_INT);
+
+$return = $objSmtm -> execute();
+
+if($return){
+    header('Location: ../web/src/views/usuario.php');
+    $_SESSION['erro'] = false;
+    $_SESSION['msgusu'] = 'Registro salvo com sucesso!';
+    exit(); 
+}else{
+    header('Location: ../web/src/views/usuario.php'); 
+    $_SESSION['erro'] = true;
+    $_SESSION['msgusu'] = 'Erro ao salvar cadastro, tente novamente mais tarde!';
+    exit();
+} 
+
